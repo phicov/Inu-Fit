6923a6efcabc94172ebc0371e74377548a67db11
Philippe Covington <philippehcbusi@gmail.com>
2023-06-06 16:12:41 -0400

    Home Page/ Hero Banner

---
+++ inu-fit/src/components/HeroBanner.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Box, Stack, Typography, Button} from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import HeroBannerImage from '../assets/images/banner.png'
+
+const HeroBanner = () => {
+  return (
+    <Box sx={{
+      mt: { lg: '212px', xs: '70px'}, 
+      ml: { sm: '50px'}
+    }} position="relative" p="20px">
+      <Typography color="#fba328" fontWeight="600" fontSize="26px">
+        Fitness Club
+      </Typography>
+      <Typography fontWeight={700}
+      sx={{ fontSize: { lg: '44px', xs: '40px'}}}
+      mb="23px" mt="30px"
+      >
+        Sweat, Smile <br /> and Repeat
+      </Typography>
+      <Typography fontSize="22px" lineHeight="35px" mb={3}>
+        Check out the most effective exercises
+      </Typography>
+      <Button variant='contained' textColor='#fff' href="#exercises" sx={{ backgroundColor: '#fba328', padding: '10px'}}>Explore Exercises</Button>
+    <Typography 
+    fontWeight={600}
+    color="#ff2625"
+    sx={{
+      opacity: 0.1,
+      display: { lg: 'block', xs: 'none'}
+    }}
+    fontSize={220}
+    >
+      Exercises
+    </Typography>
+    <img src={HeroBannerImage} alt="banner" className='hero-banner-img'/>
+    </Box>
+  )
+}
+
+export default HeroBanner
