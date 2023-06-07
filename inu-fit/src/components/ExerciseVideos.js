9fe16429684441c2d2d084782f309487dc395fc9
Philippe Covington <philippehcbusi@gmail.com>
2023-06-07 15:12:17 -0400

    Exercise Videos/ Youtube API Fix

--- inu-fit/src/components/ExerciseVideos.js
+++ inu-fit/src/components/ExerciseVideos.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-const ExerciseVideos = () => {
+const ExerciseVideos = ({ exerciseVideos, name }) => {
+  if (!exerciseVideos.length) return "Loading..";
   return (
-    <div>ExerciseVideos</div>
-  )
-}
+    <Box sx={{ marginTop: { lg: "200px", xs: "20px" } }} p="20px">
+      <Typography variant="h4" mb="33px" >
+        Watch{" "}
+        <span style={{ color: "#ff2625", textTransform: "capitalize" }}>
+          {name}
+        </span>{" "}
+        Exercise Videos
+      </Typography>
+      <Stack
+        sx={{ flexDirection: { lg: "row" }, gap: { lg: "110px", xs: "0px" } }}
+        justifyContent="flex-start"
+        flexWrap="wrap"
+        alignItems="center"
+      >
+        {exerciseVideos?.slice(0, 3)?.map((item, index) => (
+          <a
+            key={index}
+            className="exercise-video"
+            href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+            <Box>
+              <Typography variant="h5" color="#000">
+                {item.video.title}
+              </Typography>
+              <Typography variant="h6" color="#6e6e6d">
+                {item.video.channelName}
+              </Typography>
+            </Box>
+          </a>
+        ))}
+      </Stack>
+    </Box>
+  );
+};
 
-export default ExerciseVideos
+export default ExerciseVideos;
