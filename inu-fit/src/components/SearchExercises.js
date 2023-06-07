8ab1dffd2dfb67bbcfc9f830877b69aba5ffe09e
Philippe Covington <philippehcbusi@gmail.com>
2023-06-07 12:23:59 -0400

    Horizontal Scroll Bar/ Body Part Props

--- inu-fit/src/components/SearchExercises.js
+++ inu-fit/src/components/SearchExercises.js
@@ -16,10 +16,10 @@ let theme = createTheme({
   },
 });
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState('')
-  const [exercises, setExercises] = useState([])
   const [bodyParts, setBodyParts] = useState([])
+  
 
   useEffect(() => {
     const fetchExercisesData = async () => {
@@ -93,7 +93,7 @@ const SearchExercises = () => {
         </ThemeProvider>
       </Box>
       <Box sx={{ position: 'relative', width: '100%', p: '20px'}}>
-        <HorizontalScrollbar data={bodyParts} />
+        <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
       </Box>
     </Stack>
   );
